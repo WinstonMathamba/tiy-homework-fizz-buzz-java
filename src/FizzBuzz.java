ee91b0bebf015486a4a419357d2408cf98e6d6c0
WinstonMathamba <winstonmathamba@gmail.com>
2016-08-05 16:07:53 -0400

    Adding a bit more..

---
+++ src/FizzBuzz.java
@@ -0,0 +1,14 @@
+/**
+ * Created by win808mac on 8/5/16.
+ */
+public class FizzBuzz {
+
+    public void doIt(int start, int end) {
+
+        for(int x = 0; x <= 100 ; x = x + 1 ) {
+            System.out.print(x);
+        }
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by win808mac on 8/5/16.
+ */
+public class Main {
+
+    FizzBuzz firstBuzz = new FizzBuzz()
+
+}
