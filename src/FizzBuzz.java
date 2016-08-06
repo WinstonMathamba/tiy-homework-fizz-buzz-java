b039e32fff05f8a43b99a775394bc77e001c5bd1
WinstonMathamba <winstonmathamba@gmail.com>
2016-08-06 12:22:44 -0400

    Another commit includes format updates

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,14 +1,40 @@
-/**
- * Created by win808mac on 8/5/16.
- */
-public class FizzBuzz {
+    /**
+     * Created by win808mac on 8/5/16.
+     */
+    public class FizzBuzz {
 
-    public void doIt(int start, int end) {
+        public void doIt(int start, int end) {
 
-        for(int x = 0; x <= 100 ; x = x + 1 ) {
-            System.out.print(x);
+            for(int x = start; x <= end; x = x + 1)
+            {
+
+
+                // For numbers which are multiples of both three and five print "FizzBuzz".
+                if ((x%3 == 0) && (x%5 == 0))
+                {
+                    System.out.print("FizzBuzz ");
+                }
+                // For multiples of three print "Fizz" instead of the number.
+                else if (x%3 == 0)
+                {
+                    System.out.print("Fizz ");
+                }
+
+                //For the multiples of five it print "Buzz".
+               else  if (x%5 == 0)
+                {
+                    System.out.print("Buzz ");
+                }
+                else
+                    {
+                    System.out.print(x);
+                    }
+
+                System.out.print(" ");
+
+            }
+
         }
+
+
     }
-
-
-}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@
  */
 public class Main {
 
-    FizzBuzz firstBuzz = new FizzBuzz()
+    public static void main (String[] args) {
+
+        FizzBuzz firstBuzz = new FizzBuzz();
+        // instantiated an instance of FizzBuzz under "firstBuzz"
+
+        firstBuzz.doIt(1, 100);
+        // I'm calling on method doIt and assigning arguments start and end
+
+    }
 
 }
